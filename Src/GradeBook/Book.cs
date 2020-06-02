bb1882fa0f3a1be281692fe691d83b995cd79043
Elizabeth luong <elizabethluong@live.co.uk>
2020-06-02 13:50:48 +0100

    Add data validation and letter grade

--- Src/GradeBook/Book.cs
+++ Src/GradeBook/Book.cs
@@ -8,29 +8,82 @@ namespace GradeBook
         public Book(string name)
         {
             grades = new List<double>();
-            this.name = name;
+            Name = name;
         }
+
+        /*
+        public void AddLetterGrad(char letter)
+        {
+            switch (letter)
+            {
+                case 'A':
+                    AddGrade(90);
+                    break;
+                case 'B':
+                    AddGrade(80);
+                    break;
+                case 'C':
+                    AddGrade(70);
+                    break;
+                case 'D':
+                    AddGrade(60);
+                    break;
+                case 'E':
+                    AddGrade(50);
+                    break;
+                    default:
+                        AddGrade(0);
+                        break;
+            }
+        }
+        */
 
         public void AddGrade(double grade)
         {
-            grades.Add(grade);
+            if (grade <= 100 && grade >= 0)
+            {
+                grades.Add(grade);
+            }
+            else
+            {
+                Console.Write("Invalid Value");
+            }
         }
         
         public Statistics GetStatistics()
         {
             var result = new Statistics {Average = 0.0, High = double.MinValue, Low = double.MaxValue};
-            foreach(var grade in grades)
+            for(var index = 0; index < grades.Count; index += 1)
             {
-                result.Low = Math.Min(grade, result.Low);
-                result.High = Math.Max(grade, result.High);
-                result.Average += grade;
+                result.Low = Math.Min(grades[index], result.Low);
+                result.High = Math.Max(grades[index], result.High);
+                result.Average += grades[index];
             }
             result.Average /= grades.Count;
+            switch (result.Average)
+            {
+                case var d when d >= 90.0:
+                    result.Letter = 'A';
+                    break;
+                case var d when d >= 80.0:
+                    result.Letter = 'B';
+                    break;
+                case var d when d >= 70.0:
+                    result.Letter = 'C';
+                    break;
+                case var d when d >= 60.0:
+                    result.Letter = 'D';
+                    break;
+                default:
+                    result.Letter = 'F';
+                    break;
+            }
+
             return result;
         }
         
         private List<double> grades;
-        private string name;
+        public string Name;
         
     }
 }
