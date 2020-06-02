f6a990e14aecca9bfb7bd2213e9a4a6cb3a0e941
Elizabeth luong <elizabethluong@live.co.uk>
2020-06-02 14:02:13 +0100

    Remove hard coded grades and add user input functionality

--- Src/GradeBook/Program.cs
+++ Src/GradeBook/Program.cs
@@ -8,9 +8,21 @@ namespace GradeBook
         static void Main(string[] args)
         {
             var book = new Book("liz's Grade Book");
-            book.AddGrade(89.1);
-            book.AddGrade(90.5);
-            book.AddGrade(101);
+            
+            while(true)
+            {
+                Console.WriteLine("Enter a grade of 'q' to quit");
+                var input = Console.ReadLine();
+                
+                if(input == "q")
+                {
+                    break;
+                }
+
+                var grade = double.Parse(input);
+                book.AddGrade(grade);
+            }
+            
             var stats = book.GetStatistics();
             Console.WriteLine($"The average grade is {stats.Average}");
             Console.WriteLine($"The lowest grade is {stats.Low}");
