b273a5b9de07d16440467f148a5d24afebe98bf5
Elizabeth luong <elizabethluong@live.co.uk>
2020-06-05 10:19:37 +0100

    Refactor InMemoryBook.cs and move switch into Statistics.cs

--- Src/GradeBook/InMemoryBook.cs
+++ Src/GradeBook/InMemoryBook.cs
@@ -60,28 +60,7 @@ namespace GradeBook
         for (var index = 0; index < grades.Count; index += 1)
             {
                 result.Add(grades[index]);
-            }
-
-            switch (result.Average)
-            {
-                case var d when d >= 90.0:
-                    result.Letter = 'A';
-                    break;
-                case var d when d >= 80.0:
-                    result.Letter = 'B';
-                    break;
-                case var d when d >= 70.0:
-                    result.Letter = 'C';
-                    break;
-                case var d when d >= 60.0:
-                    result.Letter = 'D';
-                    break;
-                default:
-                    result.Letter = 'F';
-                    break;
-            }
-
-            return result;
+            } return result;
         }
 
         private List<double> grades;
